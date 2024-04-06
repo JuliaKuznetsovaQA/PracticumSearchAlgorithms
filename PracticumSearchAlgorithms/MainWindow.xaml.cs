32082138f42f7550b0dd58a2a1d72026f12e07f2
Julia <kuznetsovajulia@gmail.com>
2024-04-06 19:37:34 +0300

    Добавьте файлы проекта.

---
+++ PracticumSearchAlgorithms/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PracticumSearchAlgorithms
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            textbox1.Text = "";
+            textbox2.Text = "";
+            textbox3.Text = "";
+
+            int[] a = new int[10];
+
+            Random random = new Random();
+
+            textbox2.Text = "Hi!";
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = random.Next(100);
+                textbox1.Text += a[i] + "\n";
+            }
+
+            /* int a = 5;
+            int b = 3;
+            int c = a;
+
+            a = b;
+            b = c;
+
+            textbox1.Text = "a = " + a + ", b = " + b; */
+        }
+    }
+}
